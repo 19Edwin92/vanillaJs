1bd3973f5baedfdfeb93ba63b23d8429445eb761
19Edwin92 <b_a_s_e@naver.com>
2023-02-13 16:32:53 +0900

    vanilaJsVer02132

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const h1 = document.querySelector(".hello h1");
 
 function clickEvent() {
-  if(h1.className === "active") {
+  const clickedClass = "clicked"
+  if(h1.classList.contains(clickedClass)) {
     h1.className = "";
   } else {
-    h1.className = "active"
+    h1.classList = clickedClass;
   }
 }
 
