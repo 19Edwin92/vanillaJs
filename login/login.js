dfc1d1e88f1e222fe5e53e9ee9f371afdd1c3836
19Edwin92 <b_a_s_e@naver.com>
2023-02-23 11:21:54 +0900

    ver10

--- login/login.js
+++ login/login.js
@@ -5,7 +5,12 @@ const loginInput = document.querySelector('#loginForm input');
 function onLoginSubmit(tomato) {
   tomato.preventDefault();
   console.log(tomato);
+  const username = loginInput.value;
+  loginForm.classList.add("hidden")
+  // .add 클래스를 추가해 준다. 
+  document.querySelector('h1[class="hidden"]').classList.remove('hidden')
 }
+
 loginForm.addEventListener("submit", onLoginSubmit)
 
 
